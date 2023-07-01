c077e830443fbf037fe26e8645745eccc243adcc
Ji Seungmin <cswcsm02@gmail.com>
2023-07-01 23:13:52 +0900

    [chore]: mongodb 라이브러리 추가

--- index.js
+++ index.js
@@ -9,8 +9,44 @@ const port = process.env.PORT;
 
 const resource_url = process.env.OAUTH_URL;  // 스프링 서버의 보호된 리소스 URL
 
-/*dialog api*/
-app.get('/dialog', async (req, res) => {
+//mongoose
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // 연결이 성공적으로 이루어졌을 때의 콜백
+  console.log("Connected to MongoDB!");
+});
+
+
+/*oauth api*/
+//Oauth2.0 연결 API
+app.get('/oauth2', async (req, res) => {
+  const token = req.headers.authorization;  // 헤더에서 액세스 토큰 추출
+  
+  //리소스에 접급
+  try {
+    const response = await axios.get(resource_url, {
+      headers: {
+        'Authorization': `${token}`
+      }
+    });
+
+    // 응답 전송
+    const data = response.data;
+    
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send(error.toString());
+  }
+});
+
+/*chat room api*/
+//채팅방 생성 API
+app.get('/chat/room', async (req, res) => {
   const token = req.headers.authorization;  // 헤더에서 액세스 토큰 추출
   
   //리소스에 접급
