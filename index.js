ea2c0e16f8f2986fff0df27ab672cf4afaf7e478
Ji Seungmin <cswcsm02@gmail.com>
2023-07-02 15:25:38 +0900

    [feat]: delete api 기능 구현

--- index.js
+++ index.js
@@ -181,6 +181,66 @@ app.get('/chat/room/:roomId', async (req, res) => {
   }
 });
 
+//채팅방 삭제 API
+app.delete('/chat/room/:roomId/leave', async (req, res) => {
+  const token = req.headers.authorization;  // 헤더에서 액세스 토큰 추출
+  const roomId = req.params.roomId;  // route parameter로부터 roomId를 추출
+
+  //리소스에 접급
+  try {
+    const response = await axios.get(resource_url, {
+      headers: {
+        'Authorization': `${token}`
+      }
+    });
+
+    // 유저 정보 수집
+    const me = response.data.userId
+
+    // Chatroom 찾기
+    chatroom.findOne({ roomId: roomId })
+    .then((chatroomToLeave) => {
+      if(chatroomToLeave) {
+        // 해당 채팅방의 participants에 me가 있다면 삭제
+        if(chatroomToLeave.participants.includes(me)) {
+          chatroomToLeave.participants.pull(me);
+          // participants가 비어있다면 채팅방 삭제
+          if(chatroomToLeave.participants.length === 0) {
+            chatroomToLeave.remove()
+            .then(() => {
+              res.json({message: `Room with id ${roomId} deleted successfully`});  // 채팅방 삭제 성공 메시지 전송
+            })
+            .catch((err) => {
+              console.error(err);
+              res.status(500).send(err.toString());
+            });
+          } else {
+            chatroomToLeave.save()
+            .then(() => {
+              res.json({message: `User with id ${me} left the room successfully`});  // 참여자 리스트에서 제거 성공 메시지 전송
+            })
+            .catch((err) => {
+              console.error(err);
+              res.status(500).send(err.toString());
+            });
+          }
+        } else {
+          res.status(400).send('User not found in the room');  // 참여자 리스트에 없는 경우
+        }
+      } else {
+        res.status(404).send('Room not found');  // roomId에 해당하는 방이 없는 경우
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(err.toString());
+    });
+  } catch (error) {
+    res.status(500).send(error.toString());
+  }
+});
+
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
 });
