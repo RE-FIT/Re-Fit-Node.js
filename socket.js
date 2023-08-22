21d5b488ea7e89f5b21b04fe1c53d21cd72b4fbc
Ji Seungmin <cswcsm02@gmail.com>
2023-08-23 02:15:16 +0900

    [feat]: 채팅방 나갈시 채팅방 나간 시간 체크라는 기능 추가

--- socket.js
+++ socket.js
@@ -123,8 +123,19 @@ module.exports = (io) => {
         });
 
         // 클라이언트가 방을 나갈 때 실행할 이벤트 핸들러
-        socket.on('leaveRoom', (roomId, userId) => {
+        socket.on('leaveRoom', async (roomId, userId) => {
             socket.leave(roomId);
+
+            const room = await chatroom.findOne({ roomId: roomId });
+            if (room) {
+                if (userId === room.buyer) {
+                    room.buyer_out = new Date();
+                } else if (userId === room.seller) {
+                    room.seller_out = new Date();
+                }
+                await room.save();
+            }
+
             console.log(`User ${userId} left room ${roomId}`);
         });
 
